772956b11a8bf2dd6d91c94e7a215a5f212d11d0
Baxturns <eltocinosbvyt@gmail.com>
2026-08-16 18:20:10 -0500

    Load shared Explore styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -20,6 +20,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     links: [
       { rel: "stylesheet", href: appCss },
       { rel: "stylesheet", href: "/retro2012.css" },
+      { rel: "stylesheet", href: "/explore.css" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" },
