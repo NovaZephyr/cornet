1c043083897a0d285a788200a3eda63aec93f073
Baxturns <eltocinosbvyt@gmail.com>
2026-09-12 02:29:51 -0500

    restore

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,1 @@
-import { useQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
-import { AppShell } from "@/components/AppShell";
-import { supabase } from "@/integrations/supabase/client";
-import { VideoCard } from "@/components/VideoCard";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <AppShell><div>Home</div></AppShell>;
-}
+RESTORE_AFTER_FETCH
