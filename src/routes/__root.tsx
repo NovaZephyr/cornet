ad3dbb27b8f6d285e3dfac9b0e855a44d473a6de
Baxturns <eltocinosbvyt@gmail.com>
2026-09-12 02:29:26 -0500

    Restore root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,1 +1,25 @@
-TEMP
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { AppShell } from "@/components/AppShell";
+import { RouteContentBoundary } from "@/components/root/RouteContentBoundary";
+import { MaintenanceGate } from "@/components/root/MaintenanceGate";
+import { GlobalMobileBottomBar } from "@/components/root/GlobalMobileBottomBar";
+import { NotificationPermissionPrompt } from "@/components/root/NotificationPermissionPrompt";
+import "@/styles.css";
+
+export const Route = createRootRoute({
+  component: RootLayout,
+});
+
+function RootLayout() {
+  return (
+    <MaintenanceGate>
+      <AppShell>
+        <RouteContentBoundary>
+          <Outlet />
+        </RouteContentBoundary>
+        <GlobalMobileBottomBar />
+        <NotificationPermissionPrompt />
+      </AppShell>
+    </MaintenanceGate>
+  );
+}
