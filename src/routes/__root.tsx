666229fbc097ca1583a8a682afa00b2b81d085da
Baxturns <eltocinosbvyt@gmail.com>
2026-09-13 01:43:26 -0500

    Isolate root to render Outlet only

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,4 @@
-import { createRootRoute, Outlet, useRouter } from "@tanstack/react-router";
-import { QueryClientProvider, type QueryClient } from "@tanstack/react-query";
-import { AppShell } from "@/components/AppShell";
-import { RouteContentBoundary } from "@/components/root/RouteContentBoundary";
-import { MaintenanceGate } from "@/components/root/MaintenanceGate";
-import { GlobalMobileBottomBar } from "@/components/root/GlobalMobileBottomBar";
-import { NotificationPermissionPrompt } from "@/components/root/NotificationPermissionPrompt";
-import { DesignLibraryRuntime } from "@/design-library/DesignLibraryRuntime";
-import { AuthProvider } from "@/hooks/useAuth";
-import { ThemeProvider } from "@/hooks/useTheme";
-import "@/styles.css";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   head: () => ({
@@ -21,25 +11,5 @@ export const Route = createRootRoute({
 });
 
 function RootLayout() {
-  const router = useRouter();
-  const queryClient = (router.options.context as { queryClient: QueryClient }).queryClient;
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <AuthProvider>
-          <DesignLibraryRuntime />
-          <MaintenanceGate>
-            <AppShell>
-              <RouteContentBoundary>
-                <Outlet />
-              </RouteContentBoundary>
-              <GlobalMobileBottomBar />
-              <NotificationPermissionPrompt />
-            </AppShell>
-          </MaintenanceGate>
-        </AuthProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
-  );
+  return <Outlet />;
 }
