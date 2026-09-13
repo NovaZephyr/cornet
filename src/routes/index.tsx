9ac9e2cd7f1c4ce72317aead7bab4848193cc4ba
Baxturns <eltocinosbvyt@gmail.com>
2026-09-13 01:49:58 -0500

    Isolate home route for render diagnostics

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { AppShell } from "@/components/AppShell";
-import { supabase } from "@/integrations/supabase/client";
-import { VideoCard } from "@/components/VideoCard";
 
-export const Route = createFileRoute("/")({ component: Home });
+export const Route = createFileRoute("/")({
+  component: HomeRoute,
+});
 
-function Home() {
-  return <AppShell><div>Home</div></AppShell>;
+function HomeRoute() {
+  return <div>Cornet / funciona</div>;
 }
